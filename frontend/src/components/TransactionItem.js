5623d0dc5148f6f0e6a3fae7f7397faf4ba31bfa
naveengitboi <naveenjangiti9@gmail.com>
2024-03-10 23:24:56 +0530

    added transaction draft page

--- frontend/src/components/TransactionItem.js
+++ frontend/src/components/TransactionItem.js
@@ -2,17 +2,28 @@ import React from 'react'
 import '../componentStyles/TransactionItem.css'
 function TransactionItem() {
     return (
-        <div className='tsItem'>
-            <div className="userProfile">
-                <img src="/profile.png" alt="user profile" />
+        <>
+            <div className="tsItemsHeader">
+                <p className='pSmall'>Profile</p>
+                <p className='pSmall'>from</p>
+                <p className='pSmall'>type</p>
+                <p className='pSmall'>Date</p>
+                <p className='pSmall'>Through</p>
+                <p className='pSmall'>Amount</p>
+                <p className='pSmall'>Action</p>
             </div>
-            <p className='pMedium'>Naveen</p>
-            <p className='pMedium'>Received</p>
-            <p className='pMedium'>12/02/24</p>
-            <p className='pMedium'>Paytm</p>
-            <p className='pMedium'>550</p>
-            <button className='actionBtn defaultBtn'>Remove</button>
-        </div>
+            <div className='tsItem'>
+                <div className="userProfile">
+                    <img src="/profile.png" alt="user profile" />
+                </div>
+                <p className='pMedium'>Naveen</p>
+                <p className='pMedium'>Received</p>
+                <p className='pMedium'>12/02/24</p>
+                <p className='pMedium'>Paytm</p>
+                <p className='pMedium'>550</p>
+                <button className='actionBtn defaultBtn'>Remove</button>
+            </div>
+        </>
     )
 }
 
