8b2ef12ebb331665b624e787d0e34c8aa0001133
NaveenCodes <110653456+naveengitboi@users.noreply.github.com>
2024-05-23 22:20:35 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
-
+// https://walletmanagerapp-api.vercel.app/
+//http://localhost:3001
 const api = axios.create({
-    baseURL: 'http://localhost:3001/api/',
+    baseURL: 'https://walletmanagerapp-api.vercel.app/api/',
     headers: {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
