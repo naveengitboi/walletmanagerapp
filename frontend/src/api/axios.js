254c47b271e9754384dc492e46f780b88dee6d9b
naveengitboi <naveenjangiti9@gmail.com>
2024-05-27 21:11:01 +0530

    added url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -4,9 +4,8 @@ import axios from 'axios'
 
 const api = axios.create({
     baseURL: 'https://walletmanagerapp-api.vercel.app/api',
-    // baseURL: 'http://localhost:3001/api',
     headers: {
-        "Access-Control-Allow-Origin": "https://walletmanagerapp-api.vercel.app/api",
+        "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
         'Access-Control-Allow-Credentials': true,
     }
